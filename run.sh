c4b286b3b2b9922ded86e883a16097557b83580b
Philipp Schrader <philipp.schrader@gmail.com>
2014-10-12 05:13:13 +0000

    Remove libc copy and fix permissions for tmux socket.
    Now that I'm using the 14.04 image as the docker base, I don't have to
    worry about bringing in libc anymore.
    
    Also, in order to let the guest user from within docker access the socket
    I needed to change the permissions to R/W by everyone.

--- run.sh
+++ run.sh
@@ -6,7 +6,6 @@ set -u
 readonly TMUX_BIN="$(type -P tmux)"
 readonly SSH_PORT_FORWARD=${SSH_PORT_FORWARD:-999}
 readonly GENERIC_NAME=tmux-demo-port-"${SSH_PORT_FORWARD}"
-readonly LIBC6_LIB="$(ldd "${TMUX_BIN}" | awk '/libc.so.6/{print $3}')"
 
 # Make sure that tmux is installed.
 if [[ -z "${TMUX_BIN}" ]]; then
@@ -17,23 +16,24 @@ fi
 
 readonly TEMP_DIR="$(mktemp -d)"
 
-echo "Copying tmux and libc to temporary location..."
+echo "Copying tmux to temporary location..."
 cp -f "${TMUX_BIN}" "${TEMP_DIR}"/tmux
-cp -f "${LIBC6_LIB}" "${TEMP_DIR}"/libc.so.6
 
-echo "Creating tmux session called ${GENERIC_NAME}..."
+echo "Creating tmux session at socket ${TEMP_DIR}/${GENERIC_NAME} ..."
 tmux -S "${TEMP_DIR}/${GENERIC_NAME}" \
     new-session -d \
     -s "${GENERIC_NAME}"
+
+echo "Letting others join the tmux session..."
+chmod o+rw "${TEMP_DIR}/${GENERIC_NAME}"
 
 echo "Creating docker instance..."
 docker run \
     --name "${GENERIC_NAME}" \
     -p "${SSH_PORT_FORWARD}:22" \
     -v "${TEMP_DIR}/tmux:/usr/local/bin/tmux" \
-    -v "${TEMP_DIR}/libc.so.6:/usr/local/lib/libc.so.6" \
     -v "${TEMP_DIR}/${GENERIC_NAME}:/var/lib/tmux-sessions/guest" \
-    tmux-guest
+    tmux-guest || :
 
 echo "Destroying docker instance..."
 docker rm "${GENERIC_NAME}"
